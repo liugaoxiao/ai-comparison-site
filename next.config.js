92338cc587b57281f0eecbe9f260c38b13452580
liugaoxiao <134051180+liugaoxiao@users.noreply.github.com>
2026-08-05 13:36:25 +0800

    feat: 添加核心配置文件

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [{protocol: "https", hostname: "**"}]
+  },
+  experimental: {serverActions: true}
+};
+module.exports = nextConfig;
